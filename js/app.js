8e52a8e4f8b674f7f784827e163911ce43e5d5b5
ebere10 <ebere10@hotmail.com>
2016-12-10 15:00:21 +0000

    added event listeners to the keys and start button

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log('loaded');
+console.log('hi');
+window.onload = start;
+
+
+var keys = document.getElementsByTagName('li');
+
+function start(){
+
+  for (var i = 0; i < keys.length; i++) {
+    keys[i].addEventListener('click', function() {
+      console.log(this);
+    });
+  }
+
+  var startGame = document.getElementById('start');
+  startGame.addEventListener('click', randomSequence);
+
+  function randomSequence() {
+    console.log(this);
+  }
+
+}
