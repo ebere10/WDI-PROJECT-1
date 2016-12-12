c2e8d6baffd925b99f8353124c7dee4f396e4c4a
ebere10 <ebere10@hotmail.com>
2016-12-12 08:23:55 +0000

    added music files, changed it all to jquery, named the functions needed for the app but not filled them yet

--- js/app.js
+++ js/app.js
@@ -1,22 +1,59 @@
-console.log('hi');
-window.onload = start;
+$(startGame);
+
+var $computerSequence = [];
+var $playerGuess = [];
+var $sequenceLength = 3;
+
+var $keys = $('li');
+
+function startGame(){
+
+  var $start = $('#start');
+  $start.on('click', randomSequence);
+
+  //why doesn't it work when- var $keys = $(li) -is defined globally and not in the function?
+  var $keys = $('li');
+  $keys.on('click', function() {
+    var $this = $(this);
+    $this.toggleClass('illuminated');
+    var audio = new Audio('piano_notes/' + $this.attr('id') + '.wav');
+    audio.play();
+// console.log(this, "click");
+    setTimeout(function() {
+      $this.toggleClass('illuminated');
+    }, 500);
+  });
+}
 
 
-var keys = document.getElementsByTagName('li');
-
-function start(){
-
-  for (var i = 0; i < keys.length; i++) {
-    keys[i].addEventListener('click', function() {
-      console.log(this);
-    });
+function randomSequence() {
+  for (var i = 0; i < $sequenceLength; i++) {
+    $computerSequence.push(computerRandomKey());
   }
+  playSequence();
+}
 
-  var startGame = document.getElementById('start');
-  startGame.addEventListener('click', randomSequence);
+function computerRandomKey() {
+  return Math.ceil(Math.random() * ($keys).length);
+}
 
-  function randomSequence() {
-    console.log(this);
+function playSequence() {
+//computer plays a random sequence
+
+  playerInput();
+}
+
+function playerInput() {
+  if ($playerGuess.length === $sequenceLength) {
+    checkForMatch();
   }
+}
 
+function checkForMatch() {
+  var $result = $playerGuess === $computerSequence;
+  if ($result) {
+    //computer choose another random key and push it into the computerSequence array - then playSequence again
+  } else {
+    //game over and reset
+  }
 }
